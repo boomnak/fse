69cfb4466e74de15aafff936006a42ddeea53b75
adamv <addes3@live.com>
2016-06-18 21:43:56 -0400

    Added some files that weren't uploaded.

---
+++ saveload.lua
@@ -0,0 +1,51 @@
+-- saveload.lua
+-- Contains functions for saving and loading of saved games.
+
+local Serpent = require 'lib.serpent.src.serpent'
+
+local Event = require 'event'
+
+local SaveLoad = {}
+
+function SaveLoad.save(game, saveFile)
+  local data = {}
+  
+  -- Retreive the data necessecary for the save file.
+  data.vars = Event.sandbox.vars
+  data.player = game.player
+  
+  local player
+  -- Find the player in the current entities.
+  for i = 1, #game.entities do
+    if game.entities[i].name == 'player' then
+      player = game.entities[i]
+      break
+    end
+  end
+  data.player.x, data.player.y = player.pos.x, player.pos.y
+  
+  -- Serialize the data.
+  local sdata = 'return ' .. Serpent.block(data)
+  
+  game.player.x, game.player.y = nil, nil
+  
+  -- Write the serialized data to the save file.
+  love.filesystem.write(saveFile, sdata, sdata:len())
+end
+
+function SaveLoad.load(game, saveFile)
+  local data = {}
+  
+  -- Retreive the data from the savefile and load it.
+  data = love.filesystem.load(saveFile)()
+  
+  Event.sandbox.vars = data.vars
+  game.player = data.player
+  
+  local x, y = data.player.x, data.player.y
+  game.player.x, game.player.y = nil, nil
+  
+  game:switchMap('overworld', x, y)
+end
+
+return SaveLoad
